b673df4c45450b1f3b022709e65be6c84b37dc43
Sawera Sajid <sawerasajid60@gmai.com>
2026-05-28 10:27:45 -0700

    db connected

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import connectDB from "./config/db.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,3 +13,5 @@ app.listen(port, () => {
     connectDB();
     console.log("server is running on", port)
 })
+
+//openrouter_api_key= "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"
